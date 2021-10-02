21d1bf1a1fa517ae9e678e49f069e88542bbbd7b
Niels Larmuseau <niels@larmuseau.org>
2021-10-02 12:18:32 +0200

    init

---
+++ events/interactionCreate.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'interactionCreate',
+	execute(interaction) {
+        if (!interaction.isCommand()) return;
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'Issue executing commmand, alert niels', ephemeral: true });
+        }	
+    },
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client,Collection, Intents } = require('discord.js');
+const { token,DBHOST,DBPASS } = require('./config.js');
+
+// Create a new client instance
+class ClientDecorator extends Client{
+    constructor(){
+        super({ intents: [Intents.FLAGS.GUILDS] });
+        this.DBconnection = mysql.createPool({
+            connectionLimit : 10,
+            host            : DBHOST,
+            user            : 'root',
+            password        : DBPASS,
+            database        : 'discordstats'
+          });
+    }
+}
+const client = new ClientDecorator();
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Login to Discord with your client's token
+client.login(token);
